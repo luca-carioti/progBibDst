3fd3a944835ba45a1e9c09dc294a29fc6bfcaa6a
luca-carioti <80411603+luca-carioti@users.noreply.github.com>
2022-11-15 15:56:22 +0100

    First commit

---
+++ src/test/java/com/example/progbibdst/ProgBibDstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progbibdst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgBibDstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
